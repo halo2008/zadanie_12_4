b49e28a7fa664c0f5aa8f3592e33eb534e12f246
halo2008 <ksedkowski@gmail.com>
2017-05-22 18:19:12 +0200

    new script

--- js/script.js
+++ js/script.js
@@ -1,38 +1,18 @@
-var prefix = "https://cors-anywhere.herokuapp.com/";
-var tweetLink = "https://twitter.com/intent/tweet?text=";
-var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var url = 'https://api.icndb.com/jokes/random';
+var button = document.getElementById('get-joke');
 
-function getQuote() {
-    $.getJSON(prefix + quoteUrl, createTweet);
-}
-
-function createTweet(input) {
-    var data = input[0];
-    var quoteText = $(data.content).text().trim();
-    var quoteAuthor = data.title;  
-    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-    
-    if (tweetText.length > 140) {
-		getQuote();
-    }
-	
-	else {
-		var tweet = tweetLink + encodeURIComponent(tweetText);
-		$('.quote').text(quoteText);
-		$('.author').text("Author: " + quoteAuthor);
-		$('.tweet').attr('href', tweet);
-	}
-  
-    if (!quoteAuthor.length) {
-        quoteAuthor = "Unknown author";
-    }
-}
-
-$(document).ready(function() {
-    getQuote();
-    $('.trigger').click(function() {
-        getQuote();
-    })
+button.addEventListener('click', function(){
+    getJoke();
 });
 
-$.ajaxSetup({ cache: false });
+var paragraph = document.getElementById('joke');
+
+function getJoke() {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url);
+	xhr.addEventListener('load', function(){
+		var response = JSON.parse(xhr.response);
+    	paragraph.innerHTML = response.value.joke;
+	});
+    xhr.send();
+}
