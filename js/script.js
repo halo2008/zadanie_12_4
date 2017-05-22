e5f24eeaf5abfa42947a7686f4403b501810cab9
halo2008 <ksedkowski@gmail.com>
2017-05-22 18:20:50 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var url = 'https://api.icndb.com/jokes/random';
 var button = document.getElementById('get-joke');
 
-button.addEventListener('click', function(){
+button.addEventListener('click', function() {
     getJoke();
 });
 
@@ -10,7 +10,7 @@ var paragraph = document.getElementById('joke');
 function getJoke() {
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', url);
-	xhr.addEventListener('load', function(){
+	xhr.addEventListener('load', function() {
 		var response = JSON.parse(xhr.response);
     	paragraph.innerHTML = response.value.joke;
 	});
